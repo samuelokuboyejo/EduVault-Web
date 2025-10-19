4690ec65d1b1e5c491cfecbe8cf3fd62b9af99e0
samuelokuboyejo <sammyokuboyejo@gmail.com>
2025-10-19 12:52:17 +0100

    Refresh token

--- lib/api.ts
+++ lib/api.ts
@@ -42,8 +42,10 @@ api.interceptors.response.use(
           refreshToken,
         })
 
-        const { accessToken } = response.data
+        const { accessToken, refreshToken: newRefreshToken } = response.data
         localStorage.setItem("accessToken", accessToken)
+        localStorage.setItem("refreshToken", newRefreshToken)
+
 
         originalRequest.headers.Authorization = `Bearer ${accessToken}`
         return api(originalRequest)
