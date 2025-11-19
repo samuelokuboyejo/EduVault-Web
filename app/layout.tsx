b3f624c13334de0b4020301b4494dc1eb0ef44fb
Samuel Okuboyejo <samuel@spotflow.one>
2025-11-19 05:56:01 +0100

    `Update metadata icon from logo1.svg to logo2.png`

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
   title: 'EduVault',
   description: 'Sign in and manage your account',
   icons: {
-    icon: "/logo1.svg",
+    icon: "/logo2.png",
   },
 }
 
